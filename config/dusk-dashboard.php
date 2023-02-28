30f30a4db6d1ee77834d1368ab80c9c8eabf1be6
Nicholas.Ramage <nicholas.ramage@health.qld.gov.au>
2023-02-28 14:40:34 +1000

    fix: added branch

--- config/dusk-dashboard.php
+++ config/dusk-dashboard.php
@@ -2,7 +2,7 @@
 
 return [
     /*
-     * You may specify which host the dashboard runs on in case it differs from the APP_URL
+     * You may specify which hosts the dashboard runs on in case it differs from the APP_URL
      */
     'host' => env('DUSK_DASHBOARD_URL', env('APP_URL', 'http://localhost')),
 ];
